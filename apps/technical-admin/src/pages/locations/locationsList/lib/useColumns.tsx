9731781b07180fbda0c2ba82af198fd5acae4080
parisa <parisaghorbany77@gmail.com>
2024-12-10 16:54:11 +0330

    Add a table cell with an Edit link to the future Location Details page.

--- apps/technical-admin/src/pages/locations/locationsList/lib/useColumns.tsx
+++ apps/technical-admin/src/pages/locations/locationsList/lib/useColumns.tsx
@@ -1,24 +1,43 @@
 import { DataTableDropdown, DataTableSearchInput } from '@nexus-ui/ui'
 import { FilterMatchMode } from 'primereact/api'
+import { Button } from 'primereact/button'
 import { ColumnProps } from 'primereact/column'
+import { Link } from 'react-router-dom'
 
-import {  LocationEntity } from '@/entities/location'
+import { LocationEntity } from '@/entities/location'
+import { pageUrls } from '@/shared/lib'
 import { StatusCell } from '@/shared/ui'
 
-
 export const useColumns = () => {
- 
-
+  const linkTemplate = (entity: LocationEntity) => {
+    return (
+      <Link to={pageUrls.locations.edit(entity.id)}>
+        <Button
+          link
+          label={'Bearbeiten'}
+          className="capitalize text-theme-primary"
+          icon="pi pi-pencil"
+          text
+          iconPos="right"
+        />
+      </Link>
+    )
+  }
   const columns: ColumnProps[] = [
     {
       field: 'area.id',
-      header: "Area-ID",
+      header: 'Area-ID',
       sortable: true,
       filter: true,
       filterMatchMode: FilterMatchMode.CONTAINS,
       filterElement: DataTableSearchInput,
       showFilterMenu: false,
       showClearButton: false,
+      pt: {
+        bodyCell: {
+          className: 'text-center ',
+        },
+      },
     },
     {
       field: 'id',
@@ -29,10 +48,15 @@ export const useColumns = () => {
       filterElement: DataTableSearchInput,
       showFilterMenu: false,
       showClearButton: false,
+      pt: {
+        bodyCell: {
+          className: 'text-center ',
+        },
+      },
     },
     {
       field: 'name',
-      header: "Standortname",
+      header: 'Standortname',
       sortable: true,
       filter: true,
       filterMatchMode: FilterMatchMode.CONTAINS,
@@ -43,13 +67,13 @@ export const useColumns = () => {
     {
       field: 'brands.name',
       filterMatchMode: FilterMatchMode.CUSTOM,
-      header: "Marke",
+      header: 'Marke',
       sortable: true,
       filter: true,
-     
+
       showFilterMenu: false,
       showClearButton: false,
-      
+
       pt: {
         headerCell: {
           className: 'min-w-64',
@@ -58,7 +82,7 @@ export const useColumns = () => {
     },
     {
       field: 'address.postCode',
-      header: "Postleitzahl",
+      header: 'Postleitzahl',
       sortable: true,
       filter: true,
       filterMatchMode: FilterMatchMode.CONTAINS,
@@ -68,7 +92,7 @@ export const useColumns = () => {
     },
     {
       field: 'address.city',
-      header: "Stadt",
+      header: 'Stadt',
       sortable: true,
       filter: true,
       filterMatchMode: FilterMatchMode.CONTAINS,
@@ -78,7 +102,7 @@ export const useColumns = () => {
     },
     {
       field: 'address.address',
-      header: "Adresse",
+      header: 'Adresse',
       sortable: true,
       filter: true,
       filterMatchMode: FilterMatchMode.CONTAINS,
@@ -87,40 +111,47 @@ export const useColumns = () => {
       showClearButton: false,
     },
     {
-        field: 'address.country.name',
-        header: "Land",
-        sortable: true,
-        filter: true,
-        filterMatchMode: FilterMatchMode.CONTAINS,
-        filterElement: DataTableSearchInput,
-        showFilterMenu: false,
-        showClearButton: false,
+      field: 'address.country.name',
+      header: 'Land',
+      sortable: true,
+      filter: true,
+      filterMatchMode: FilterMatchMode.CONTAINS,
+      filterElement: DataTableSearchInput,
+      showFilterMenu: false,
+      showClearButton: false,
+    },
+    {
+      field: 'isActive',
+      filterMatchMode: FilterMatchMode.EQUALS,
+      header: 'Status',
+      body: (location: LocationEntity) => <StatusCell data={location} />,
+      sortable: true,
+      filter: true,
+      filterElement: DataTableDropdown({
+        options: [
+          {
+            label: 'Active',
+            value: true,
+          },
+          {
+            label: 'inActive',
+            value: false,
+          },
+        ],
+        optionLabel: 'label',
+        optionValue: 'value',
+      }),
+      showFilterMenu: false,
+      showClearButton: false,
+    },
+    {
+      body: linkTemplate,
+      pt: {
+        bodyCell: {
+          className: 'text-end ',
+        },
       },
-      {
-        field: 'isActive',
-        filterMatchMode: FilterMatchMode.EQUALS,
-        header: "Status",
-        body: (location: LocationEntity) => <StatusCell data={location} />,
-        sortable: true,
-        filter: true,
-        filterElement: DataTableDropdown({
-          options: [
-            {
-              label: "Active",
-              value: true,
-            },
-            {
-              label: "inActive",
-              value: false,
-            },
-          ],
-          optionLabel: 'label',
-          optionValue: 'value',
-        }),
-        showFilterMenu: false,
-        showClearButton: false,
-      },
-  
+    },
   ]
 
   return columns
