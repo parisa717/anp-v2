422e0889c1cfff52a96395b0d28e347af669bccd
parisa <parisaghorbany77@gmail.com>
2024-12-10 16:18:16 +0330

    Create an locations list route & page.

--- apps/technical-admin/src/pages/locations/routes.ts
+++ apps/technical-admin/src/pages/locations/routes.ts
@@ -3,10 +3,11 @@ import { RouteObject } from 'react-router-dom'
 import { authRequiredLoader, ROUTE_PATHS } from '@/shared/lib'
 
 import { CreateLocationPage } from './createLocation/ui/Page'
+import { LocationsListPage } from './locationsList/ui/Page'
 
 export const locationsRoutes: RouteObject = {
   path: ROUTE_PATHS.Locations.Root,
-  Component: null,
+  Component: LocationsListPage,
   loader: authRequiredLoader,
   children: [
     {
