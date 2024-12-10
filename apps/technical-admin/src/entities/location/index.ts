5fc50da1da598f3ee1f7905282e24caabdc26efb
parisa <parisaghorbany77@gmail.com>
2024-12-10 16:02:17 +0330

    Use Location RTKQ API / schema from the mocked GQL server.

--- apps/technical-admin/src/entities/location/index.ts
+++ apps/technical-admin/src/entities/location/index.ts
@@ -1,3 +1,5 @@
 export { locationApi, useCreateLocationMutation } from './api/locationApi'
+export {transformLocations} from './lib/transformLocations'
 export { locationSlice } from './model/slice'
 export type { LocationEntity } from './model/types'
+
